7b714e8ec83deecb46136b272ebd0a832d89fd7b
EthanHazelSchool <ehaze002@plattsburgh.edu>
2025-11-20 14:30:15 -0500

    Finish calculator

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,39 +1,59 @@
 function App() {
-  const [currentValue, setCurrentValue] = React.useState(0);
-  const [previousValue, setPreviousValue] = React.useState(currentValue);
-  const [operation, setOperation] = React.useState(null); // plus, minus, multiply, divide
+  const inputOne = React.useRef(0);
+  const inputTwo = React.useRef(0);
+  const operation = React.useRef("plus");
 
-  function operate(op) {
-    setOperation(op);
-    setPreviousValue(currentValue);
-    setCurrentValue(null);
+  const [answer, setAnswer] = React.useState(0);
+
+  function Calculate() {
+    const one = parseInt(inputOne.current.value) || 0;
+    const two = parseInt(inputTwo.current.value) || 0;
+    const operator = operation.current.value || "plus"; // shouldn't ever trigger the || but just in case I guess
+
+    switch (operator) {
+      case "plus":
+        return setAnswer(one + two);
+      case "minus":
+        return setAnswer(one - two);
+      case "multiply":
+        return setAnswer(one * two);
+      case "divide":
+        return setAnswer(one / two);
+    }
   }
 
   return (
     <React.Fragment>
       <h1>Calculator</h1>
+      <p>
+        This calculator sucks but I didn't want to implement a number pad or
+        anything
+      </p>
       <div className="calc">
-        <span className="calc-value">{currentValue}</span>
-        <div className="calc-num">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-        </div>
-        <div className="calc-operations">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>/</button>
-          <button>=</button>
-        </div>
+        <input
+          type="number"
+          name="input-1"
+          id="input-1"
+          placeholder="0"
+          ref={inputOne}
+        />
+        <select name="operation" id="operation" ref={operation}>
+          <option value="plus">+</option>
+          <option value="minus">-</option>
+          <option value="multiply">*</option>
+          <option value="divide">/</option>
+        </select>
+        <input
+          type="number"
+          name="input-2"
+          id="input-2"
+          placeholder="0"
+          ref={inputTwo}
+        />
+        <button onClick={() => Calculate()}>=</button>
       </div>
+      <p>Invalid numbers will be treated as 0</p>
+      <p className="answer">{answer}</p>
     </React.Fragment>
   );
 }
